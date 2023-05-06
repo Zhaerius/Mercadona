b2cbbba359af57bedc3a9d49a5fc36fbfc0a5ae6
Gaetan Demazeux <gaetan.demazeux@outlook.com>
2023-05-06 17:28:28 +0200

    add article service

---
+++ src/Application.Core/Interfaces/Services/IArticleService.cs
@@ -0,0 +1,16 @@
+﻿using Application.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core.Interfaces.Services
+{
+    public interface IArticleService
+    {
+        public ArticleEntity GetArticle(Guid id);
+        public IEnumerable<ArticleEntity> GetArticles();
+        public void AddArticle(ArticleEntity article);
+    }
+}
---
+++ src/Application.Core/Services/ArticleService.cs
@@ -0,0 +1,37 @@
+﻿using Application.Core.Entities;
+using Application.Core.Interfaces.Repositories;
+using Application.Core.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core.Services
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+
+        public ArticleEntity GetArticle(Guid id)
+        {
+            return _articleRepository.GetById(id);
+        }
+
+        public IEnumerable<ArticleEntity> GetArticles()
+        {
+            return _articleRepository.GetAll();
+        }
+
+        public void AddArticle(ArticleEntity article)
+        {
+            _articleRepository.Create(article);
+        }
+    }
+}

5488866e2d84f95270b5587e9b9b77ee031a965d
Gaetan Demazeux <gaetan.demazeux@outlook.com>
2023-10-07 18:57:52 +0200

    Ajout projet test E2E

---
+++ tests/E2ETests/ModelPages/LoginPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2ETests.ModelPages
+{
+    internal class LoginPage
+    {
+        private readonly WebDriverWait _webDriverWait;
+        private By _loginInput = By.Id("login");
+        private By _passwordInput = By.Id("password");
+
+        public LoginPage(WebDriverWait webDriverWait)
+        {
+            _webDriverWait = webDriverWait;
+        }
+
+        public void EnterFieldValue(string fieldValue)
+        {
+            var field = _webDriverWait.Until<IWebElement>(ExpectedConditions.ElementIsVisible(_loginInput));
+            field.SendKeys(fieldValue);
+        }
+
+    }
+}
---
+++ tests/E2ETests/Tests/LoginPageTest.cs
@@ -0,0 +1,41 @@
+﻿using E2ETests.ModelPages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E2ETests.Tests
+{
+    [TestClass]
+    internal class LoginPageTest
+    {
+        private IWebDriver _webdriver;
+        private WebDriverWait _webDriverWait;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _webdriver = new ChromeDriver();
+            _webDriverWait = new WebDriverWait(_webdriver, TimeSpan.FromSeconds(20));
+            _webdriver.Navigate().GoToUrl("https://localhost:7060/");
+        }
+
+        [TestMethod]
+        public void HomeLoginTest()
+        {
+            var loginPage = new LoginPage(_webDriverWait);
+            loginPage.EnterFieldValue("gaetan.demazeux@outlook.com");
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            Thread.Sleep(3000);
+            _webdriver.Quit();
+        }
+    }
+}
